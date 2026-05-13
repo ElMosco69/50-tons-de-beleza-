0524abe3e17c3ec3b8c167bf0028eb217eb6b451
ArtNin011 <arthursantosmendes88@gmail.com>
2026-05-13 12:26:01 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -11,31 +11,38 @@
 	const DB_DSN = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8";
 
 	/** Usuário do banco de dados MySQL */
-	define('DB_USER', 'root');
+	// define('DB_USER', 'root');
+	const DB_USER = 'root';
 
 	/** Senha do banco de dados MySQL */
-	define('DB_PASSWORD', '');
+	// define('DB_PASSWORD', '');
+	const DB_PASSWORD = '';
 
 
 	/** caminho absoluto para a pasta do sistema **/
-	if (!defined('ABSPATH'))
-		define('ABSPATH', dirname(__FILE__) . '/');
+	const ABSPATH =  __DIR__ . '/';
+	// if (!defined('ABSPATH'))
+	// 	define('ABSPATH', dirname(__FILE__) . '/');
 
 	/** caminho no server para o sistema **/
-	if (!defined('BASEURL'))
-		define('BASEURL', '/50-tons-de-beleza-/');
+	const BASEURL = '/50-tons-de-beleza-/';
+	// if (!defined('BASEURL'))
+	// 	define('BASEURL', '/50-tons-de-beleza-/');
 
 	/** caminho do arquivo de banco de dados **/
-	if (!defined('DBAPI'))
-		define('DBAPI', ABSPATH . 'inc/database.php');
+	const DBAPI = ABSPATH . 'inc/database.php';
+	// if (!defined('DBAPI'))
+	// 	define('DBAPI', ABSPATH . 'inc/database.php');
 
 	/** caminhos dos templates de header e footer **/
-	define('HEADER_TEMPLATE', ABSPATH . 'inc/header.php');
-	define('FOOTER_TEMPLATE', ABSPATH . 'inc/footer.php');
+	const HEADER_TEMPLATE = ABSPATH . 'inc/header.php';
+	const FOOTER_TEMPLATE = ABSPATH . 'inc/footer.php';
+	// define('HEADER_TEMPLATE', ABSPATH . 'inc/header.php');
+	// define('FOOTER_TEMPLATE', ABSPATH . 'inc/footer.php');
 
 
 	/// caminho da imagem padrão para perfis sem foto
-
-	define('IMAGE_TEMPLATE', BASEURL . 'img/Logo_50tons.png');
+	const IMAGE_TEMPLATE = BASEURL .'img/Logo_50tons.png';
+	// define('IMAGE_TEMPLATE', BASEURL . 'img/Logo_50tons.png');
 
 ?>
