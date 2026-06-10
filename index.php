efd235113eed9e8d4cbed9a9daddb4cd6a37182e
Gi-Menon <gigimaria998@gmail.com>
2026-06-10 12:20:06 -0300

    Nome bacanudo

--- index.php
+++ index.php
@@ -118,7 +118,90 @@
 				<div class="row bora">
 					<div class="col-8 calma a">
  						<div class="calabreso aviso-card">
-
+							<div class="row align-items-center g-3">
+								<div class="col-auto aviso-thumb">
+									<img class="img-fluid rounded" src="<?php echo BASEURL; ?>img/aviso1.jpg" alt="Aviso">
+								</div>
+								<div class="col aviso-body">
+									<h2>dia das pamonhas onhas</h2>
+									<p class="aviso-text mb-0">
+										A história começou
+										quando um relógio esquisito
+										Grudou no pulso dele vindo la do infinito
+										Agora tem poderes e com eles faz bonito
+										É o Ben 10
+										Se acaso encontra-lo você vai se admirar
+										Diante de seus olhos ele vai se transformar
+										Em um ser alienígina
+										Que bota pra quebrar
+										É o Ben 10
+										Com seus poderes vai combater
+										Os inimigos e vai vencer
+										Ele não foge de medo ou dor
+										Moleque muito irado
+									Seja onde for	É o Ben 10
+															A história começou
+										quando um relógio esquisito
+										Grudou no pulso dele vindo la do infinito
+										Agora tem poderes e com eles faz bonito
+										É o Ben 10
+										Se acaso encontra-lo você vai se admirar
+										Diante de seus olhos ele vai se transformar
+										Em um ser alienígina
+										Que bota pra quebrar
+										É o Ben 10
+										Com seus poderes vai combater
+										Os inimigos e vai vencer
+										Ele não foge de medo ou dor
+										Moleque muito irado
+									Seja onde for	É o Ben 10
+									</p>
+								</div>
+							</div>
+						</div>
+					<div class="calabreso aviso-card">
+							<div class="row align-items-center g-3">
+								<div class="col-auto aviso-thumb">
+									<img class="img-fluid rounded" src="<?php echo BASEURL; ?>img/aviso1.jpg" alt="Aviso">
+								</div>
+								<div class="col aviso-body">
+									<h2>dia das pamonhas onhas</h2>
+									<p class="aviso-text mb-0">
+										A história começou
+										quando um relógio esquisito
+										Grudou no pulso dele vindo la do infinito
+										Agora tem poderes e com eles faz bonito
+										É o Ben 10
+										Se acaso encontra-lo você vai se admirar
+										Diante de seus olhos ele vai se transformar
+										Em um ser alienígina
+										Que bota pra quebrar
+										É o Ben 10
+										Com seus poderes vai combater
+										Os inimigos e vai vencer
+										Ele não foge de medo ou dor
+										Moleque muito irado
+									Seja onde for	É o Ben 10
+															A história começou
+										quando um relógio esquisito
+										Grudou no pulso dele vindo la do infinito
+										Agora tem poderes e com eles faz bonito
+										É o Ben 10
+										Se acaso encontra-lo você vai se admirar
+										Diante de seus olhos ele vai se transformar
+										Em um ser alienígina
+										Que bota pra quebrar
+										É o Ben 10
+										Com seus poderes vai combater
+										Os inimigos e vai vencer
+										Ele não foge de medo ou dor
+										Moleque muito irado
+									Seja onde for	É o Ben 10
+									</p>
+								</div>
+							</div>
+						</div>
+ 						<div class="calabreso aviso-card">
 							<div class="row align-items-center g-3">
 								<div class="col-auto aviso-thumb">
 									<img class="img-fluid rounded" src="<?php echo BASEURL; ?>img/aviso1.jpg" alt="Aviso">
@@ -166,6 +249,46 @@
 							<h3> 
  								TODOS
 							</h3>
+							<div class="accordion" id="accordionExample">
+								<div class="accordion-item">
+									<h2 class="accordion-header">
+									<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+										HOJE 
+									</button>
+									</h2>
+									<div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+									<div class="accordion-body">
+										<div class="deiz">
+											<p>OLAAA</p>
+										</div>
+									</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header">
+									<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+										Accordion Item #2
+									</button>
+									</h2>
+									<div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+									<div class="accordion-body">
+										<strong>This is the second item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+									</div>
+									</div>
+								</div>
+								<div class="accordion-item">
+									<h2 class="accordion-header">
+									<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+										Accordion Item #3
+									</button>
+									</h2>
+									<div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+									<div class="accordion-body">
+										<strong>This is the third item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+									</div>
+									</div>
+								</div>
+								</div>
 						</div>
 					</div>
 				</div>
