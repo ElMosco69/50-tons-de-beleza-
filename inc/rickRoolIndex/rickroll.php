80a13fa11a0fe74f5a29f1519a359968a5acc963
elMosco69 <matheusviniramos2020@gmail.com>
2026-08-06 21:30:38 -0300

    atualização misterioooooosa

---
+++ inc/rickRoolIndex/rickroll.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <style>
+            body {
+                background-image: url("nevergonnaletyougo.gif");
+                 background-repeat: no-repeat;
+                 background-size: cover;
+                 background-attachment: fixed;
+            }
+        </style>
+    </head>
+    <body>
+        <audio src="nevergonna.MP3" type="audio/MP3" allow="autoplay" id="audio" style="display:none"></audio>
+    </body>
+</html>
